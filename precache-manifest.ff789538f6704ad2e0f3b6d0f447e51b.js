f615929098c72f0358f1d12a056733aac0a8cccb
Dzulfikrie <espadamoba@gmail.com>
2020-04-13 10:09:20 +0700

    Menyalin isi folder build

---
+++ precache-manifest.ff789538f6704ad2e0f3b6d0f447e51b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a7d3c3cc537c8d3bac67297b4e9536a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c850901aa62bf13c1600",
+    "url": "/static/css/main.e98904e7.chunk.css"
+  },
+  {
+    "revision": "8b774d6fc0a533a33407",
+    "url": "/static/js/2.846bd9ac.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.846bd9ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c850901aa62bf13c1600",
+    "url": "/static/js/main.b3d08c5c.chunk.js"
+  },
+  {
+    "revision": "0bc84ee4062e33ac79d5",
+    "url": "/static/js/runtime-main.9462ffd4.js"
+  }
+]);
